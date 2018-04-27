c2bced357884a91433f965d2436584aae1915204
zhangjunwen <zhangjunwen@codemao.cn>
2018-04-27 20:45:03 +0800

    temp: stash

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,13 @@ const config_happypack = require('./webpack/happypack.config');
 
 let webpack_config =  merge.smart(config_common, config_happypack);
 
-// if (process.env.NODE_ENV === 'production') {
-//   const config_prod = require('./webpack/prod.config.js');
-//   webpack_config = merge.smart(webpack_config, config_prod)
-// } else {
-//   const config_dev = require('./webpack/dev.config.js');
-//   webpack_config = merge.smart(webpack_config, config_dev)
-// }
+if (process.env.NODE_ENV === 'production') {
+  const config_prod = require('./webpack/prod.js');
+  webpack_config = merge.smart(webpack_config, config_prod)
+} else {
+  const config_dev = require('./webpack/dev.js');
+  webpack_config = merge.smart(webpack_config, config_dev)
+}
 
-const config_dev = require('./webpack/dev.js');
-webpack_config = merge.smart(webpack_config, config_dev)
 
 module.exports = webpack_config;
--- webpack/config.js
+++ webpack/config.js
@@ -3,12 +3,13 @@ const path = require('path');
 module.exports = {
   ENTRY: path.resolve(__dirname, '../src/index.tsx'),
   DIST_PATH:path.resolve(__dirname, '../dist'),
-  PUBLIC_PATH: '/',
+  PUBLIC_PATH: './',
   SRC_PATH: path.resolve(__dirname, '../src'),
   INDEX_HTML: 'index.html',
   ROOT_PATH: path.resolve(__dirname, '..'),
   HTML_TEMPLATE_PATH: path.resolve(__dirname, '../src/index.html'),
   DLL_PATH: path.resolve(__dirname, '../webpack/dll'),
+  PUBLIC_PATH_ASSET: path.resolve(__dirname, '../dist/assets'),
   LIBRARIES: [
     'react',
     'react-dom',
