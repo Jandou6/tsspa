b8a4992bb578500923f5cc7e7090c31b04b3e026
jandou <809781809@qq.com>
2018-07-12 18:40:24 +0800

    fix: public path in webpack config

--- template/webpack/config.js
+++ template/webpack/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   ENTRY: path.resolve(__dirname, '../src/index.tsx'),
   DIST_PATH:path.resolve(__dirname, '../dist'),
-  PUBLIC_PATH: './',
+  PUBLIC_PATH: '/',
   SRC_PATH: path.resolve(__dirname, '../src'),
   INDEX_HTML: 'index.html',
   ROOT_PATH: path.resolve(__dirname, '..'),
