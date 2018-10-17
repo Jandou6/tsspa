223e0123b999d5699ccdc828bb53033632701c35
jandou <809781809@qq.com>
2018-10-17 16:15:00 +0800

    fix: add max-age

--- template/bin/server.js
+++ template/bin/server.js
@@ -3,8 +3,16 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const mime = require('mime-types')
 
-app.use(express.static(path.resolve(__dirname,'../dist')));
+app.use(express.static(path.resolve(__dirname,'../dist'),  {
+  maxAge: '1y',
+  setHeaders: function (res, path) { //skip *.html
+    if (mime.lookup(path) === 'text/html') {
+      res.setHeader('Cache-Control', 'public, max-age=0')
+    }
+  }
+}));
 app.use(function (req, res) {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 })
